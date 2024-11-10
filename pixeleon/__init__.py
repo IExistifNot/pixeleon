023715a1748193f259f60c56e41ea5380bab0c5f
JackieT <65199465+IExistifNot@users.noreply.github.com>
2024-11-10 10:48:21 -0500

    Update __init__.py

--- pixeleon/__init__.py
+++ pixeleon/__init__.py
@@ -1,23 +1,30 @@
 # Import core functionalities from different modules
-from .core import Screen, Sprite, COLORS, initialize, redraw
+from .core import Screen, Sprite, initialize, redraw
 from .game_loop import GameLoop, handle_key_down, handle_key_up
 from .utilities import insert_to_plist, sort_plist
+from .config import SCALE, WIDTH, HEIGHT, BG_COLOR, RETRY_LIMIT, UP_INT, colors
 
 # Define __all__ to specify what is publicly accessible
 __all__ = [
     "Screen",
     "Sprite",
-    "COLORS",
     "initialize",
     "redraw",
     "GameLoop",
     "handle_key_down",
     "handle_key_up",
     "insert_to_plist",
-    "sort_plist"
+    "sort_plist",
+    "SCALE",
+    "WIDTH",
+    "HEIGHT",
+    "BG_COLOR",
+    "RETRY_LIMIT",
+    "UP_INT",
+    "colors"
 ]
 
 # Set the package metadata
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "Jack Tiedemann"
 __license__ = "MIT"  # Assuming you've chosen MIT license
