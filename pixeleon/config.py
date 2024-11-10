9e419175f688210cb71e8443d19dc2ef0e83b891
JackieT <65199465+IExistifNot@users.noreply.github.com>
2024-11-10 09:51:13 -0500

    Update config.py

--- pixeleon/config.py
+++ pixeleon/config.py
@@ -1,3 +1,10 @@
+SCALE = 3 # Screen display will be scaled by this factor. Will not affect proportions.
+WIDTH = 180
+HEIGHT = 120
+BG_COLOR = "2" # can be any color in the colors dictionary aside from #
+RETRY_LIMIT = 10  # Max retries
+UP_INT = 30 # intervals for game updates in ms
+
 colors = {
     "0": (255, 0, 0), "1": (0, 255, 0), "2": (0, 0, 255),
     "3": (255, 255, 0), "4": (0, 255, 255), "5": (255, 165, 0),
