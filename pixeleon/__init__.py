f0b13ddea344c309715164c5ccd8426c18714df1
JackieT <65199465+IExistifNot@users.noreply.github.com>
2024-11-10 11:24:01 -0500

    Update __init__.py

--- pixeleon/__init__.py
+++ pixeleon/__init__.py
@@ -1,29 +1,29 @@
 # Import core functionalities from different modules
-# from .core import *
-# from .utilities import *
-# from .game_loop import *
-# from .config import *
-# from .assets.sprites import *
+from .core import *
+from .utilities import *
+from .game_loop import *
+from .config import *
+from .assets.sprites import *
 
 # Define __all__ to specify what is publicly accessible
-# __all__ = [
-#     "Screen",
-#     "Sprite",
-#     "initialize",
-#     "redraw",
-#     "GameLoop",
-#     "handle_key_down",
-#     "handle_key_up",
-#     "insert_to_plist",
-#     "sort_plist",
-#     "SCALE",
-#     "WIDTH",
-#     "HEIGHT",
-#     "BG_COLOR",
-#     "RETRY_LIMIT",
-#     "UP_INT",
-#     "colors"
-# ]
+__all__ = [
+    "Screen",
+    "Sprite",
+    "initialize",
+    "redraw",
+    "GameLoop",
+    "handle_key_down",
+    "handle_key_up",
+    "insert_to_plist",
+    "sort_plist",
+    "SCALE",
+    "WIDTH",
+    "HEIGHT",
+    "BG_COLOR",
+    "RETRY_LIMIT",
+    "UP_INT",
+    "colors"
+]
 
 # Set the package metadata
 __version__ = "1.0.0"
